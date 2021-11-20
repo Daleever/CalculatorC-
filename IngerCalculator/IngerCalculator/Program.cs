7645e32aca5915e6b692b72fa77872dcaf1bcb24
Daleever <daleever1994@gmail.com>
2021-11-20 15:48:50 +0200

    Zinch1

---
+++ IngerCalculator/IngerCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class AdvancedCalc : OrdinaryCalc
+{
+    public double TranslateToKByte(int Byte)
+    {
+        if(Byte < 1)
+        {
+            throw new Exception("Введено значение меньше 1");
+        }
+        return (double)Byte / 1024;
+    }
+}
