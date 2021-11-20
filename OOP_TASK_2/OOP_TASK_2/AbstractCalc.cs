1822024802f5df901e13be570e5db3ba692df497
deadmadini <kolesnik23215@gmail.com>
2021-11-20 18:42:16 +0200

    Kolesnyk6

---
+++ OOP_TASK_2/OOP_TASK_2/AbstractCalc.cs
@@ -0,0 +1,11 @@
+abstract class AbstractCalc
+{
+    abstract public double GetNum1();
+    abstract public void SetNum1(double num1);
+    abstract public double GetNum2();
+    abstract public void SetNum2(double num2);
+    abstract public double Add();
+    abstract public double Sub();
+    abstract public double Div();
+    abstract public double Mul();
+}
--- OOP_TASK_2/OOP_TASK_2/sCalculator.cs
+++ OOP_TASK_2/OOP_TASK_2/sCalculator.cs
@@ -1,29 +1,29 @@
 using System;
 
-class OrdinaryCalc{
+class OrdinaryCalc:AbstractCalc{
 	protected double num1 = 0;
 	protected double num2 = 0;
-	public double GetNum1(){
+	public override double GetNum1(){
 		return num1;
 	}
-	public void SetNum1(double num1){
+	public override void SetNum1(double num1){
 		this.num1 = num1;
 	}
-	public double GetNum2(){
+	public override double GetNum2(){
 		return num2;
 	}
-	public void SetNum2(double num2){
+	public override void SetNum2(double num2){
 		this.num2 = num2;
 	}
-	public double Add()
+	public override double Add()
 	{
 		return num1 + num2;
 	}
-	public double Sub()
+	public override double Sub()
 	{
 		return num1 - num2;
 	}
-	public double Div()
+	public override double Div()
 	{
 		if (num2 == 0)
 		{
@@ -34,7 +34,7 @@ class OrdinaryCalc{
 			return num1 / num2;
 		}
 	}
-	public double Mul()
+	public override double Mul()
 	{
 		return num1 * num2;
 	}
