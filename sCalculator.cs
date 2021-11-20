048f47fb8eecea8375fa8f528daa3a20312e563c
deadmadini <kolesnik23215@gmail.com>
2021-11-20 16:11:34 +0200

    Kolesnyk1

---
+++ sCalculator.cs
@@ -0,0 +1,33 @@
+class OrdinaryCalc{
+	private double num1 = 0;
+	private double num2 = 0;
+	public double GetNum1(){
+		return num1;
+	}
+	public void SetNum1(double num1){
+		this.num1 = num1;
+	}
+	public double GetNum2(){
+		return num2;
+	}
+	public void SetNum2(double num2){
+		this.num2 = num1;
+	}
+
+	public double Add()
+	{
+		return num1 + num2;
+	}
+	public double Sub()
+	{
+		return num1 - num2;
+	}
+	public double Div()
+	{
+		return num1 / num2;
+	}
+	public double Mul()
+	{
+		return num1 * num2;
+	}
+}
