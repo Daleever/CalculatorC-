854152f4cc1eeaf154437a074127a2eab88c0007
Daleever <daleever1994@gmail.com>
2021-11-20 16:49:35 +0200

    Zinich4

--- IngerCalculator/IngerCalculator/Main.cs
+++ IngerCalculator/IngerCalculator/Main.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Lb_4
+namespace Lb_5
 {
     class Program
     {
-        static void Main()
+        static public void Main()
         {
 
+            
+            Console.WriteLine("Вы запустили калькулятор");
+            Console.WriteLine("Выберите режим:");
+            Console.WriteLine("1.Обычный     |     2.Инженерный");
+            bool isIngener = Console.Read() == '1' ? false : true ;
+            Console.WriteLine(isIngener);
+
+            while(true)
+            {
+                try
+                {
+
+
+                }catch(DivideByZeroException e)
+                {
+                    Console.WriteLine(e);
+                }
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
         }
     }
 }
